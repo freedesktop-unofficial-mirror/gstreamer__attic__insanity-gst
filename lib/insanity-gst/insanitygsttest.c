71b5e9822b6ad966f585cf451a5a94b74eef865b
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-02-23 16:08:01 +0000

    New insanity-gst project for gst specific insanity stuff
    Includes a sample external element generator nicked from
    the previous Python based insanity.

---
+++ lib/insanity-gst/insanitygsttest.c
@@ -0,0 +1,127 @@
+/* Insanity QA system
+
+       insanitygsttest.h
+
+ Copyright (c) 2012, Collabora Ltd <vincent@collabora.co.uk>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA.
+*/
+/**
+ * SECTION:insanitygsttest
+ * @short_description: GStreamer Test
+ * @see_also: #InsanityTest
+ *
+ * %TODO.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <insanity-gst/insanitygsttest.h>
+
+G_DEFINE_TYPE (InsanityGstTest, insanity_gst_test,
+    INSANITY_TYPE_THREADED_TEST);
+
+struct _InsanityGstTestPrivateData
+{
+  int dummy;
+};
+
+#if 0
+static gboolean
+insanity_gst_test_setup (InsanityTest *test)
+{
+  if (!INSANITY_THREADED_TEST_CLASS (insanity_gst_test_parent_class)->setup (test))
+    return FALSE;
+
+  printf("insanity_gst_test_setup\n");
+
+  return TRUE;
+}
+
+static gboolean
+insanity_gst_test_start (InsanityTest *test)
+{
+  if (!INSANITY_THREADED_TEST_CLASS (insanity_gst_test_parent_class)->start (test))
+    return FALSE;
+
+  printf("insanity_gst_test_start\n");
+
+  return TRUE;
+}
+
+static void
+insanity_gst_test_stop (InsanityTest *test)
+{
+  printf("insanity_gst_test_stop\n");
+
+  INSANITY_THREADED_TEST_CLASS (insanity_gst_test_parent_class)->stop (test);
+}
+
+static void
+insanity_gst_test_teardown (InsanityTest *test)
+{
+  printf("insanity_gst_test_teardown\n");
+
+  INSANITY_THREADED_TEST_CLASS (insanity_gst_test_parent_class)->teardown (test);
+}
+#endif
+
+static void
+insanity_gst_test_init (InsanityGstTest * test)
+{
+  InsanityGstTestPrivateData *priv = G_TYPE_INSTANCE_GET_PRIVATE (test,
+      INSANITY_TYPE_GST_TEST, InsanityGstTestPrivateData);
+
+  test->priv = priv;
+}
+
+static void
+insanity_gst_test_class_init (InsanityGstTestClass * klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  InsanityTestClass *test_class = INSANITY_TEST_CLASS (klass);
+
+#if 0
+  test_class->setup = &insanity_gst_test_setup;
+  test_class->start = &insanity_gst_test_start;
+  test_class->stop = &insanity_gst_test_stop;
+  test_class->teardown = &insanity_gst_test_teardown;
+#endif
+
+  g_type_class_add_private (klass, sizeof (InsanityGstTestPrivateData));
+}
+
+/**
+ * insanity_gst_test_new:
+ * @name: the short name of the test.
+ * @description: a one line description of the test.
+ * @full_description: (allow-none): an optional longer description of the test.
+ *
+ * This function creates a new GStreamer test with the given properties.
+ *
+ * Returns: (transfer full): a new #InsanityGstTest instance.
+ */
+InsanityGstTest *
+insanity_gst_test_new (const char *name, const char *description, const char *full_description)
+{
+  InsanityGstTest *test = g_object_new (insanity_gst_test_get_type (),
+      "name", name, "desc", description, NULL);
+  if (full_description)
+    g_object_set (test, "full-desc", full_description, NULL);
+  return test;
+}
---
+++ lib/insanity-gst/insanitygsttest.h
@@ -0,0 +1,79 @@
+/* Insanity QA system
+
+       insanitygsttest.h
+
+ Copyright (c) 2012, Collabora Ltd <slomo@collabora.co.uk>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA 02111-1307, USA.
+*/
+
+#ifndef INSANITY_GST_TEST_H_GUARD
+#define INSANITY_GST_TEST_H_GUARD
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <insanity/insanitydefs.h>
+#include <insanity/insanitythreadedtest.h>
+
+typedef struct _InsanityGstTest InsanityGstTest;
+typedef struct _InsanityGstTestClass InsanityGstTestClass;
+typedef struct _InsanityGstTestPrivateData InsanityGstTestPrivateData;
+
+/**
+ * InsanityGstTest:
+ *
+ * The opaque #InsanityGstTest data structure.
+ */
+struct _InsanityGstTest {
+  InsanityThreadedTest parent;
+
+  /*< private >*/
+  InsanityGstTestPrivateData *priv;
+
+  gpointer _insanity_reserved[INSANITY_PADDING];
+};
+
+/**
+ * InsanityGstTestClass:
+ * @parent_class: the parent class structure
+ * @test: Start the test
+ *
+ * Insanity GStreamer test class. Override the vmethods to customize
+ * functionality.
+ */
+struct _InsanityGstTestClass
+{
+  InsanityThreadedTestClass parent_class;
+
+  /*< private >*/
+  gpointer _insanity_reserved[INSANITY_PADDING];
+};
+
+InsanityGstTest *insanity_gst_test_new(const char *name, const char *description, const char *full_description);
+
+/* Handy macros */
+#define INSANITY_TYPE_GST_TEST                (insanity_gst_test_get_type ())
+#define INSANITY_GST_TEST(obj)                (G_TYPE_CHECK_INSTANCE_CAST ((obj), INSANITY_TYPE_GST_TEST, InsanityGstTest))
+#define INSANITY_GST_TEST_CLASS(c)            (G_TYPE_CHECK_CLASS_CAST ((c), INSANITY_TYPE_GST_TEST, InsanityGstTestClass))
+#define IS_INSANITY_GST_TEST(obj)             (G_TYPE_CHECK_TYPE ((obj), INSANITY_TYPE_GST_TEST))
+#define IS_INSANITY_GST_TEST_CLASS(c)         (G_TYPE_CHECK_CLASS_TYPE ((c), INSANITY_TYPE_GST_TEST))
+#define INSANITY_GST_TEST_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS ((obj), INSANITY_TYPE_GST_TEST, InsanityGstTestClass))
+
+GType insanity_gst_test_get_type (void);
+
+#endif
+
