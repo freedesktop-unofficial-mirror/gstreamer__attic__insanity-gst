2bf926851af7260b5d5bca4388b9448574c13c5e
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-03-30 17:24:36 +0100

    Add a FSM system to the RTP test, and a couple sample steps

--- tests/insanity-test-gst-rtsp.c
+++ tests/insanity-test-gst-rtsp.c
@@ -25,8 +25,23 @@
 #include <gst/rtsp-server/rtsp-server.h>
 #include <insanity-gst/insanity-gst.h>
 
+typedef enum
+{
+  NEXT_STEP_NOW,
+  NEXT_STEP_ON_PLAYING,
+  NEXT_STEP_RESTART_ON_PLAYING,
+  NEXT_STEP_ON_PAUSED,
+} NextStepTrigger;
+
 static GstElement *global_pipeline = NULL;
 static GstRTSPServer *global_server = NULL;
+static guint global_state_change_timeout = 0;
+static unsigned int global_state = 0;
+static unsigned int global_next_state = 0;
+static GstState global_waiting_on_state = GST_STATE_VOID_PENDING;
+
+static void on_ready_for_next_state (InsanityGstPipelineTest * ptest,
+    gboolean timeout);
 
 static GstPipeline*
 rtsp_test_create_pipeline (InsanityGstPipelineTest *ptest, gpointer userdata)
@@ -282,6 +297,10 @@ rtsp_test_start(InsanityTest *test)
 
   insanity_test_validate_step (test, "valid-setup", configured, NULL);
 
+  global_state = 0;
+  global_next_state = 0;
+  global_waiting_on_state = GST_STATE_PLAYING;
+
 done:
   g_value_unset (&uri);
   g_value_unset (&video_encoder);
@@ -297,9 +316,150 @@ rtsp_test_stop (InsanityTest *test)
   rtsp_test_reset_server ();
 }
 
-static void
-rtsp_test_test (InsanityTest *test)
+static gboolean
+state_change_timeout (gpointer data)
 {
+  InsanityGstPipelineTest *ptest = data;
+
+  on_ready_for_next_state (ptest, TRUE);
+
+  return FALSE;
+}
+
+static NextStepTrigger
+rtsp_test_pause (InsanityGstPipelineTest * ptest, const char *step, guintptr data)
+{
+  global_state_change_timeout =
+      g_timeout_add (1000, (GSourceFunc) & state_change_timeout, ptest);
+  gst_element_set_state(global_pipeline, GST_STATE_PAUSED);
+  return NEXT_STEP_ON_PAUSED;
+}
+
+static NextStepTrigger
+rtsp_test_play (InsanityGstPipelineTest * ptest, const char *step, guintptr data)
+{
+  global_state_change_timeout =
+      g_timeout_add (1000, (GSourceFunc) & state_change_timeout, ptest);
+  gst_element_set_state(global_pipeline, GST_STATE_PLAYING);
+  return NEXT_STEP_ON_PLAYING;
+}
+
+/*
+  This is an ordered list of steps to perform.
+  A test function needs the passed step if needed, and validate it UNLESS it returns
+  NEXT_STEP_ON_PLAYING, in which case validation will be automatically done whenever
+  next state change to PLAYING happens. A step can return NEXT_STEP_RESTART_ON_PLAYING,
+  which will cause it to be called again upon next state change to PLAYING. The step
+  function is then responsible for keeping its own private state so it can end up
+  returning something else at some point.
+*/
+static const struct
+{
+  const char *step;
+    NextStepTrigger (*f) (InsanityGstPipelineTest *, const char *, guintptr);
+  guintptr data;
+} steps[] = {
+  { "pause", &rtsp_test_pause, 0 },
+  { "play", &rtsp_test_play, 0 },
+};
+
+static gboolean
+do_next_step (gpointer data)
+{
+  InsanityGstPipelineTest *ptest = data;
+  InsanityTest *test = data;
+  NextStepTrigger next;
+
+  /* When out of steps to perform, end the test */
+  if (global_state == sizeof (steps) / sizeof (steps[0])) {
+    insanity_test_done (test);
+    return FALSE;
+  }
+
+  insanity_test_printf (test, "Calling step %u/%zu (%s, data %u)\n",
+      global_state + 1, sizeof (steps) / sizeof (steps[0]),
+      steps[global_state].step, steps[global_state].data);
+  next =
+      (*steps[global_state].f) (ptest, steps[global_state].step,
+      steps[global_state].data);
+  switch (next) {
+    default:
+      g_assert (0);
+      /* fall through */
+    case NEXT_STEP_NOW:
+      global_state++;
+      g_idle_add ((GSourceFunc) & do_next_step, ptest);
+      break;
+    case NEXT_STEP_ON_PLAYING:
+      global_next_state = global_state + 1;
+      global_waiting_on_state = GST_STATE_PLAYING;
+      break;
+    case NEXT_STEP_ON_PAUSED:
+      global_next_state = global_state + 1;
+      global_waiting_on_state = GST_STATE_PAUSED;
+      break;
+    case NEXT_STEP_RESTART_ON_PLAYING:
+      global_next_state = global_state;
+      global_waiting_on_state = GST_STATE_PLAYING;
+      break;
+  }
+
+  return FALSE;
+}
+
+static void
+on_ready_for_next_state (InsanityGstPipelineTest * ptest, gboolean timeout)
+{
+  if (global_next_state != global_state) {
+    insanity_test_validate_step (INSANITY_TEST (ptest),
+        steps[global_state].step, TRUE, NULL);
+    global_state = global_next_state;
+  }
+  insanity_test_printf (INSANITY_TEST (ptest), "Got %s, going to next step\n",
+      timeout ? "timeout" : gst_element_state_get_name (global_waiting_on_state));
+  global_waiting_on_state = GST_STATE_VOID_PENDING;
+  g_idle_add ((GSourceFunc) & do_next_step, ptest);
+}
+
+static gboolean
+rtsp_test_bus_message (InsanityGstPipelineTest * ptest, GstMessage * msg)
+{
+  switch (GST_MESSAGE_TYPE (msg)) {
+    case GST_MESSAGE_STATE_CHANGED:
+      if (GST_MESSAGE_SRC (msg) == GST_OBJECT (global_pipeline)) {
+        GstState oldstate, newstate, pending;
+        gst_message_parse_state_changed (msg, &oldstate, &newstate, &pending);
+
+        if (global_state_change_timeout) {
+          g_source_remove (global_state_change_timeout);
+          global_state_change_timeout = 0;
+        }
+
+        if (pending == GST_STATE_VOID_PENDING && global_waiting_on_state == newstate) {
+          on_ready_for_next_state (ptest, FALSE);
+        }
+      }
+      break;
+    default:
+      break;
+  }
+
+  return TRUE;
+}
+
+static void
+rtsp_test_test (InsanityThreadedTest *ttest)
+{
+  if (gst_element_set_state (global_pipeline,
+          GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
+    insanity_test_done (INSANITY_TEST (ttest));
+    return;
+  }
+  if (gst_element_get_state (global_pipeline, NULL, NULL,
+          GST_SECOND) == GST_STATE_CHANGE_FAILURE) {
+    insanity_test_done (INSANITY_TEST (ttest));
+    return;
+  }
 }
 
 int
@@ -322,16 +482,21 @@ main (int argc, char **argv)
 
   insanity_test_add_checklist_item (test, "valid-setup", "The setup given in arguments makes sense", NULL);
   insanity_test_add_checklist_item (test, "server-created", "The RTSP server was created succesfully", NULL);
-  insanity_test_add_checklist_item (test, "launch-line", "The launch line gst-rtsp-server was configued with", NULL);
+  insanity_test_add_checklist_item (test, "pause", "The pipeline could be paused", NULL);
+  insanity_test_add_checklist_item (test, "play", "The pipeline could be played", NULL);
+
+  insanity_test_add_extra_info (test, "launch-line", "The launch line gst-rtsp-server was configued with");
 
   insanity_gst_pipeline_test_set_create_pipeline_function (ptest,
       &rtsp_test_create_pipeline, NULL, NULL);
+  insanity_gst_pipeline_test_set_initial_state (ptest, GST_STATE_PAUSED);
 
   g_signal_connect_after (test, "setup", G_CALLBACK (&rtsp_test_setup), 0);
   g_signal_connect_after (test, "teardown", G_CALLBACK (&rtsp_test_teardown), 0);
   g_signal_connect_after (test, "start", G_CALLBACK (&rtsp_test_start), 0);
   g_signal_connect_after (test, "stop", G_CALLBACK (&rtsp_test_stop), 0);
-  g_signal_connect_after (test, "test", G_CALLBACK (&rtsp_test_test), 0);
+  g_signal_connect_after (test, "pipeline-test", G_CALLBACK (&rtsp_test_test), 0);
+  g_signal_connect_after (test, "bus-message", G_CALLBACK (&rtsp_test_bus_message), 0);
 
   ret = insanity_test_run (test, &argc, &argv);
 
